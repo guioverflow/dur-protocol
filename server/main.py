23466f41e8a7920847729082758c5d1f89d72ae9
Guizao <gui.overflow@gmail.com>
2025-06-16 22:11:52 -0300

    feat: Lógica de send e receive na primitiva 1:1 (TCP).

--- server/main.py
+++ server/main.py
@@ -3,7 +3,6 @@ from enum import Enum
 class Operation(Enum):
     READ = "READ"
     WRITE = "WRITE"
-
     ABORT = "ABORT"
     COMMIT = "COMMIT"
 
@@ -15,11 +14,29 @@ class Server:
 
     def send(message, port):
         import socket
-        pass
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        with client_socket.connect(('localhost', port)) as conn:
+            client_socket.sendall(message.encode())
 
     def receive():
+        import socket
 
-        return (Operation.COMMIT, key, cid)
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(('localhost', self.port))
+
+        server_socket.listen(1)
+        conn, addr = server_socket.accept()
+
+        data_bytes = conn.recv(1024)
+        data = data_bytes.decode()
+
+        if type(data) != tuple or len(data) != 3:
+            return None
+
+        operation, key, cid = data
+
+        return (operation, key, cid)
 
     
     def start():
@@ -27,5 +44,4 @@ class Server:
             request = receive()
             if request[0] == Operation.COMMIT:
                 send(self.data[key], port)
-            
-            
+
