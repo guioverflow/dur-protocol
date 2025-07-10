b64dee48efc305dfb7f37696b69c23157729478f
Guizao <gui.overflow@gmail.com>
2025-07-10 00:15:23 -0300

    Versão final API do abcast.

---
+++ abcast/__init__.py
@@ -0,0 +1,11 @@
+
+import configparser
+from utils.unicast import receive, send as unicast_send
+from utils import get_sequencer
+
+def send(message):
+    seq_host, seq_port = get_sequencer()
+    unicast_send(seq_host, seq_port, message)
+
+def deliver(conn):
+    return receive(conn)
